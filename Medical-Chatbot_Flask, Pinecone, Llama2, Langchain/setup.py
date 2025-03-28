af978dc774504377afd318d351f08e9f494ad7e4
Rambabu Palla <105153161+RamsPalla@users.noreply.github.com>
2025-03-28 18:06:25 +0530

    Update setup.py

--- Medical-Chatbot_Flask, Pinecone, Llama2, Langchain/setup.py
+++ Medical-Chatbot_Flask, Pinecone, Llama2, Langchain/setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Medical Chatbot',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Rambabu Palla',
+    author_email= 'palla.r7@gmail.com',
     packages= find_packages(),
     install_requires = []
 
