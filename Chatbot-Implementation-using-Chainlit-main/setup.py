edb58c9eb1160638836b01308c2196c5d1305440
Rambabu Palla <105153161+RamsPalla@users.noreply.github.com>
2025-04-02 22:40:00 +0530

    Update setup.py

--- Chatbot-Implementation-using-Chainlit-main/setup.py
+++ Chatbot-Implementation-using-Chainlit-main/setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = "Zomato Chatbot",
     version= "0.0.0",
-    author= "Boktiar Ahmed Bappy",
-    author_email= "boktiar@ineuron.ai",
+    author= "Rambabu Palla",
+    author_email= "palla.r7@gmail.com",
     packages= find_packages(),
     install_requires=[]
 )
